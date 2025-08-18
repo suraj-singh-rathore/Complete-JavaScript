390fc5233c0abcc95d62d6477e1e73879a57a89a
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-18 23:42:42 +0530

    Added new folder Loops and Strings

---
+++ Loops and Strings/app.js
@@ -0,0 +1,35 @@
+// For Loop:-
+
+// for(let i=1; i<=10; i++){
+//     console.log("SURAJ")
+// }
+
+// Counting:-
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Reverse Counting:-
+
+// for( let i=10; i>0; i--){
+//     console.log(i);
+// }
+
+// Break:-
+
+// for (let i=1; i<=8; i++){
+//     if(i==6)
+//         break;
+//     else
+//         console.log(i);
+// }
+
+// Continue:-
+
+for(let i=1; i<=5; i++){
+    if(i==3)
+        continue;
+    else
+        console.log(i);
+}
