cb0c1dc7d0de76e3ab1ceee1adb12f4278580b36
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-18 22:53:13 +0530

    Updated files

--- Variables and DataTypes/app.js
+++ Variables and DataTypes/app.js
@@ -1,14 +1,62 @@
-// var age = 25;
+/////// var keyword //////
+
+//var age = 25;
 
 
 // if(true) {
 //     console.log(age);
 // }
 
-function solve() {
-    var age = 25;
-    console.log(age);
-}
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
 
-// console.log(age);
-solve();
+// solve();
+
+// var x= 20;
+// var x= 30;
+
+/////////  let keyword  //////
+
+// {
+// let a = 10;
+
+// console.log(a);
+// }
+
+
+// let a = 20;
+// a = 30;
+
+// let a = 10;
+// a = "suraj";
+// a = true;
+// a = null;
+
+
+//////// cosnt keyword  //////
+
+// const a = 21; 
+// console.log(a);
+
+// a = 20;
+// console.log(a);
+
+// let marks = 30;
+// marks = "Singh";
+// marks = true;
+// console.log(marks);
+
+// let marks;
+// console.log(marks);
+
+// let value = null;
+// console.log(value);
+
+
+let marks = 23435449749882745528548484447878484515489;
+console.log(marks);
+
+marks = null;
+console.log(typeof(marks));
