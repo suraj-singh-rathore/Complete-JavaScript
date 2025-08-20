fe6927799311d30f516ad3c3a664e92096af23c5
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-20 22:04:10 +0530

    learining arrays concept

---
+++ Reference Types/app.js
@@ -0,0 +1,48 @@
+// let obj = {
+//     name : "Suraj",
+//     age: 21,
+//     weight: 70,
+//     height: "5ft 8 inch",
+//     greet: function () {
+//         console.log("hello je kaise ho");
+//     }
+// };
+
+// console.log(obj)
+// obj.greet();
+
+// console.log(typeof(obj))
+
+// // Creation of array:-
+
+let arr = [1,2,3,4,5];
+
+// //Array Constructor:
+
+let brr = new Array('Sun', 1, true);
+brr.push('Singh');  
+brr.pop('Singh');
+
+brr.shift();
+brr.unshift('suraj singh');
+
+brr.push(21);
+brr.push(92);
+brr.push(45);
+brr.push(30);
+
+console.log( brr.slice(2,4));
+
+brr.splice(1,2, 'Ravi');
+
+console.log(brr);
+
+console.log(brr[2]);
+
+console.log(typeof(brr));
+
+
+// //Map:
+
+//let arr =[10,20,30,40,50];
+
