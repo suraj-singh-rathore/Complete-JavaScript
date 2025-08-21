948d4dcfbb1a1bf6b5299af068d3644bbb35c6b9
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-21 12:56:18 +0530

    file updated

---
+++ Temporal Dead Zone/app.js
@@ -0,0 +1,52 @@
+           //global scope:
+
+// let age = 14;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true) {
+//     console.log(age)
+// }
+
+// for(let i=0; i<2; i++) {
+//     console.log(age);
+// }
+
+// function SayHello() {
+//     console.log("hi", age);
+// }
+
+// SayHello();
+
+//         //Function Scope:
+
+// function sayHello() {
+//     var fullName = "Earth";
+//     console.log("Hello", name);
+// }
+
+// console.log(fullName);
+// sayHello();
+
+//  // Block scope:
+
+// {
+//     let height =180;
+// }
+
+// console.log(height);
+
+// //   Temporal Dead Zone in JS:-
+
+//  // form line no. 48 to 50 are temporal dead zone.
+
+
+console.log(marks);
+console.log("beloved")
+console.log("Hii")
+let marks = 100;
+console.log(marks);
