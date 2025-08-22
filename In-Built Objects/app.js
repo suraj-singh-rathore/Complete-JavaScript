ba09a95b866816aa8c0e48216d844ad53c22e52f
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-22 14:24:52 +0530

    learned about in-built objects in js

---
+++ In-Built Objects/app.js
@@ -0,0 +1,41 @@
+// // Math - Object:-
+
+// console.log("Hello World");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(30,493,32, 483, 24, 432,484));
+
+// console.log(Math.min(30,493,32, 483, 24, 432,484));
+
+// console.log(Math.round(1.6));
+
+// console.log(Math.floor(1.9));
+
+// console.log(Math.ceil(1.4));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+// // Date - Object:-
+
+let curr = new Date();
+
+let date = new Date('July 07 2004 04:00');
+
+let newdate = new Date(2004, 5, 22, 7);
+
+//console.log(newdate);
+
+// console.log(newdate.getDay());
+
+// console.log(newdate.getFullYear());
+
+console.log(newdate.setFullYear(2001));
+
+console.log(newdate);
