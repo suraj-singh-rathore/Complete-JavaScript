82dea9ea10825591513972ac4ea03d21383668e0
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-23 22:52:37 +0530

    DOM manipulation

---
+++ DOM Manipulation/app.js
@@ -0,0 +1,22 @@
+// let mydiv = document.querySelector('#mydiv');
+
+// let newElement = document.createElement('span');
+// newElement.textContent = 'This is suraj'
+
+// mydiv.insertAdjacentElement('afterbegin', newElement);
+
+// //mydiv.insertAdjacentElement('beforebegin', newElement);
+
+// //mydiv.insertAdjacentElement('afterend', newElement);
+
+// mydiv.insertAdjacentElement('beforeend', newElement);
+
+// //Remove child:-
+
+// let parent = document.querySelector('#mydiv');
+// let child = document.querySelector('#fpara');
+// parent.removeChild(child);
+
+let parent = child.parentElement;
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
