c54b763ce2175de7eabb05a006428aaf389c96b4
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-26 21:08:23 +0530

    Learned about the concept of events and listeners

--- Events and Listeners/event.js
+++ Events and Listeners/event.js
@@ -8,14 +8,55 @@
 // fpara.addEventListener('click', changeText);
 // //fpara.removeEventListener('click', changeText);
 
+// //||||||||||||||||||||||||||||||||||||||||||||||||||
 
-function changeText(event) {
-    console.log(event);
-    let fpara = document.getElementById('fpara');
-    fpara.textContent = "Hello Sir"
+
+// function changeText(event) {
+//     console.log(event);
+//     let fpara = document.getElementById('fpara');
+//     fpara.textContent = "Hello Sir"
+// }
+
+// let fpara = document.getElementById('fpara');
+
+// fpara.addEventListener('click', changeText);
+
+////||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// let anchorElement = document.getElementById('fanchor');
+
+// anchorElement.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     anchorElement.textContent = "Clicked"
+// });
+
+// // Avoiding too many listener:-
+
+// let paras = document.querySelectorAll('p');
+
+// for (let i=0; i<paras.length; i++) {
+//     let para = paras[i];
+//     para.addEventListener('click', function() {
+//         alert("You have Clicked on para: " + (i+1));
+//     })
+// }
+
+///////////////////////////// TARGET PROPERTY:-
+
+// let paras = document.querySelectorAll('p');
+
+function alertPara(event) {
+    // // For Span Tag:-
+    // if(event.target.nodeName === 'SPAN'){
+    alert("You have Clicked on para: " + event.target.textContent);
+   //}
 }
 
-let fpara = document.getElementById('fpara');
+// for(let i=0; i<paras.length; i++) {
+//     let para = paras[i];
+//     para.addEventListener('click', alertPara);
+// }
 
-fpara.addEventListener('click', changeText);
+let mydiv = document.getElementById('Wrap');
 
+document.addEventListener('click', alertPara);
