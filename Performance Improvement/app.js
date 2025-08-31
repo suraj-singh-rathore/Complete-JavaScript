d2e809d0d279fd14573edfa3b6c38e30f39235b6
suraj-singh-rathore <ssingh70115@gmail.com>
2025-08-31 22:07:08 +0530

    Update Changes

--- Performance Improvement/app.js
+++ Performance Improvement/app.js
@@ -1,28 +1,41 @@
- // // Code 1
-const t1 = performance.now()
+//  // // Code 1
+// const t1 = performance.now()
 
- for (let i=1; i<=100; i++) {
-    let para = document.createElement('p');
-    para.textContent = "This is Para" + i;
-    document.body.appendChild(para);
- }
+//  for (let i=1; i<=100; i++) {
+//     let para = document.createElement('p');
+//     para.textContent = "This is Para" + i;
+//     document.body.appendChild(para);
+//  }
 
- const t2 = performance.now();
+//  const t2 = performance.now();
 
- console.log("total time by code 1: " + (t2 - t1));
+//  console.log("total time by code 1: " + (t2 - t1));
 
-// //  code 2
-const t3 = performance.now();
-let mydiv = document.createElement('div');
+// // //  code 2
+// const t3 = performance.now();
+// let mydiv = document.createElement('div');
 
-for (let i=1; i<=100; i++) {
+// for (let i=1; i<=100; i++) {
+//    let para = document.createElement('p');
+//    para.textContent = "This is para " + i;
+//    mydiv.appendChild(para);
+// }
+
+// document.body.appendChild(mydiv);
+
+// const t4 = performance.now();
+
+// console.log("total time by code 2: " + (t4  -t3));
+
+
+let fragment = document.createDocumentFragment();
+
+for(let i=1; i<=100; i++) {
    let para = document.createElement('p');
    para.textContent = "This is para " + i;
-   mydiv.appendChild(para);
+   //NO reflow and No repaint for the below line
+   fragment.appendChild(para);
 }
 
-document.body.appendChild(mydiv);
-
-const t4 = performance.now();
-
-console.log("total time by code 2: " + (t4  -t3));
+// the below code line takes 1 reflow and 1 repaint
+document.body.appendChild(fragment);
