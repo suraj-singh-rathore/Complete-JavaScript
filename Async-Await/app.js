baa12647bfd0dc033bf80e5d1f572e6ca413c667
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-03 21:43:16 +0530

    update changes

--- Async-Await/app.js
+++ Async-Await/app.js
@@ -1,7 +1,23 @@
+
+// async function getData() {
+//     setTimeout(function() {
+//         console.log("I am inside Timeout block")
+//     }, 3000);
+// }
+
+// let output = getData();
+
+//// await:
+
+//fetch API :-
+
 async function getData() {
-    setTimeout(function() {
-        console.log("I am inside Timeout block")
-    }, 3000);
+
+    //get request - async
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/4');
+    // parse json - async
+    let data = await response.json();
+    console.log(data);
 }
 
 getData();
