55f813f7ab83b5823293527cf25c2c10dd9faced
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-04 08:28:34 +0530

    Completed js Closures

--- Closures in js/script.js
+++ Closures in js/script.js
@@ -1,9 +1,23 @@
-function init() {
-  var name = "Mozilla"; // name is a local variable created by init
-  function displayName() {
-    // displayName() is the inner function, that forms a closure
-    console.log(name); // use variable declared in the parent function
+// function init() {
+//   var name = "Suraj Singh"; // name is a local variable created by init
+//   function displayName() {
+//     // displayName() is the inner function, that forms a closure
+//     console.log(name); // use variable declared in the parent function
+//   }
+//   displayName();
+// }
+// init();
+
+
+function outerfunction() {
+  let name = "Suraj";
+
+  function innerfunction() {
+    console.log(name);
   }
-  displayName();
+
+  return innerfunction;
 }
-init();
+let inner = outerfunction();
+
+inner();
